e6320d5e1d0943693af184a4d5bf63825c12fad6
Camille <92187904+cavisz@users.noreply.github.com>
2022-02-04 15:05:33 -0300

    Create sc_int.java

---
+++ sc_int.java
@@ -0,0 +1,17 @@
+package teste;
+import java.util.Scanner;
+public class Teste {
+
+    
+    public static void main(String[] args) {
+              
+      Scanner sc = new Scanner(System.in)  ;
+        
+      int x;
+      x = sc.nextInt();
+      
+      System.out.println("digitou:" +x);
+      
+      sc.close();
+    }
+}
